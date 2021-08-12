0cfd845cde0acfa82ac2eee54da8638ab1201235
Kiran Subedi <subedikiran41@gmail.com>
2021-08-12 15:51:20 +0545

    Code refactor of role

--- src/main/java/com/treeleaf/blog/role/RoleServiceImp.java
+++ src/main/java/com/treeleaf/blog/role/RoleServiceImp.java
@@ -1,6 +1,9 @@
 package com.treeleaf.blog.role;
 
+import com.treeleaf.blog.exception.InternalServerErrorException;
+import com.treeleaf.blog.exception.ResourceNotFoundException;
 import com.treeleaf.blog.exception.RoleNameAlreadyExistsException;
+import com.treeleaf.blog.util.Translator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,9 @@ public class RoleServiceImp  implements  RoleService{
 
     @Autowired
     RoleRepository roleRepository;
+
+    @Autowired
+    Translator translator;
 
     /**
      * Check if name already exists
@@ -31,35 +37,40 @@ public class RoleServiceImp  implements  RoleService{
     @Override
     public void store(RoleRequest request) {
         if(nameExists(request.getName())){
-            throw  new RoleNameAlreadyExistsException("Role already exists.");
+            throw  new RoleNameAlreadyExistsException(translator.toLocale("error_role_already_exists"));
         }
         try{
             Role role = new Role();
             role.setName(request.getName());
             roleRepository.save(role);
         }catch(Exception e){
-
+            throw  new InternalServerErrorException(translator.toLocale("error_internal_server"));
         }
     }
 
     @Override
     public void update(Long id, RoleRequest request) {
-        Role role = roleRepository.findById(id).orElseThrow(()-> new RuntimeException("Role not found."));
+        Role role = roleRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException(translator.toLocale("error_role_not_found"))
+        );
         if(nameExists(request.getName())){
-            throw  new RoleNameAlreadyExistsException("Role already exists.");
+            throw  new RoleNameAlreadyExistsException(translator.toLocale("error_role_already_exists"));
         }
 
         try{
             role.setName(request.getName());
             roleRepository.save(role);
         }catch(Exception e){
-
+            throw new InternalServerErrorException(translator.toLocale("error_internal_server"));
         }
     }
 
     @Override
     public void delete(Long id) {
-        Role role = roleRepository.findById(id).orElseThrow(()-> new RuntimeException("Role not found."));
+        Role role = roleRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException(translator.toLocale("error_role_not_found"))
+        );
+
         roleRepository.delete(role);
     }
 }
